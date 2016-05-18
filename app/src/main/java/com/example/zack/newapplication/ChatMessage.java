9a277962eb2af8c1e31017206bb54da7ab3cbd4d
devin.diamond <devin.diamond@stu.henderson.kyschools.us>
2016-05-18 12:13:35 -0500

    vffdsd

---
+++ app/src/main/java/com/example/zack/newapplication/ChatMessage.java
@@ -0,0 +1,26 @@
+package com.example.zack.newapplication;
+
+/**
+ * Created by devin.diamond on 5/18/2016.
+ */
+public class ChatMessage {
+    private String author;
+    private String message;
+
+    public ChatMessage(){
+
+    }
+
+    public ChatMessage(String author, String message){
+        this.message = message;
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ app/src/main/java/com/example/zack/newapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.zack.newapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String FIREBASE_URl = "https://dazzling-torch-8082.firebaseio.com/"
+
+    private Firebase firebaseRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        firebaseRef = new Firebase(FIREBASE_URl);
+
+        EditText inputText = (EditText) findViewById(R.id.messageText);
+    }
+}
