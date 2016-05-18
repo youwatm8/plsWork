e18dee82677f0602742b03cd2094fa222934fde8
devin.diamond <devin.diamond@stu.henderson.kyschools.us>
2016-05-18 15:20:54 -0500

    vffdsd

--- app/src/main/java/com/example/zack/newapplication/MainActivity.java
+++ app/src/main/java/com/example/zack/newapplication/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.zack.newapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
 
@@ -10,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String FIREBASE_URl = "https://dazzling-torch-8082.firebaseio.com/";
 
     private Firebase firebaseRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         firebaseRef = new Firebase(FIREBASE_URl);
 
         EditText inputText = (EditText) findViewById(R.id.messageText);
+        inputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEND){
+                    sendMessage();
+                }
+                return true;
+
+            }
+        });
+
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+            });
+        }
+    public void sendMessage(){
+        EditText textInput = (EditText) findViewById(R.id.messageText);
+        String message = textInput.getText().toString();
+        if(!message.equals("")){
+            ChatMessage cMessage = new ChatMessage(message);
+            firebaseRef.push().setValue(cMessage);
+            textInput.setText("");
+        }
     }
 }
